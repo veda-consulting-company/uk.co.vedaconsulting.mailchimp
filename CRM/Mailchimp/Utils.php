3b2472142de971f7e7c0c35041d0460ccd28d5a7
Rajesh Sundararajan <rajeshrhino@gmail.com>
2014-04-28 16:54:49 +0100

    Webhooks - Subscribe/unsubscribe/Email address change/Hard bounce

--- CRM/Mailchimp/Utils.php
+++ CRM/Mailchimp/Utils.php
@@ -82,4 +82,81 @@ class CRM_Mailchimp_Utils {
     }
     return $mapper[$listID][$groupingID][$groupID];
   }
+  
+  /*
+   * Get Mailchimp group ID group name
+   */
+  static function getMailchimpGroupIdFromName($listID, $groupName) {
+    if (empty($listID) || empty($groupName)) {
+      return NULL;
+    }
+
+    $mcLists = new Mailchimp_Lists(CRM_Mailchimp_Utils::mailchimp());
+    try {
+      $results = $mcLists->interestGroupings($listID);
+    } 
+    catch (Exception $e) {
+      return NULL;
+    }
+    
+    foreach ($results as $grouping) {
+      foreach ($grouping['groups'] as $group) {
+        if ($group['name'] == $groupName) {
+          return $group['id'];
+        }
+      }
+    }
+  }
+  
+  static function getGroupIdForMailchimp($listID, $groupingID, $groupID) {
+    if (empty($listID) || empty($groupingID) || empty($groupID)) {
+      return NULL;
+    }
+
+    $query  = "
+      SELECT  entity_id
+      FROM    civicrm_value_mailchimp_settings mcs
+      WHERE   mc_list_id = %1 AND mc_grouping_id = %2 AND mc_group_id = %3";
+    $params = 
+        array(
+          '1' => array($listID , 'String'),
+          '2' => array($groupingID , 'String'),
+          '3' => array($groupID , 'String'),
+        );
+    $dao = CRM_Core_DAO::executeQuery($query, $params);
+    if ($dao->fetch()) {
+      return $dao->entity_id;
+    }
+  }
+  
+  /*
+   * Create/Update contact details in CiviCRM, based on the data from Mailchimp webhook
+   */
+  static function updateContactDetails($params) {
+    if (empty($params)) {
+      return NULL;
+    }
+
+    $contactParams = 
+        array(
+          'version'       => 3,
+          'contact_type'  => 'Individual',
+          'first_name'    => $params['FNAME'],
+          'last_name'     => $params['LNAME'],
+          'email'         => $params['EMAIL'],
+        );
+    
+    $email = new CRM_Core_BAO_Email();
+		$email->get('email', $params['EMAIL']);
+    
+    // If the Email was found.
+    if (!empty($email->contact_id)) {
+      $contactParams['id'] = $email->contact_id;
+    }
+    
+    // Create/Update Contact details
+    $contactResult = civicrm_api('Contact' , 'create' , $contactParams);
+    
+    return $contactResult['id'];
+  }
 }
