a92708abeebf1ca86e612eb75ef0f9151c0c00a3
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2014-04-28 18:22:57 +0530

    api for scheduled job

--- api/v3/Mailchimp.php
+++ api/v3/Mailchimp.php
@@ -104,3 +104,27 @@ function civicrm_api3_mailchimp_getcivicrmgroupmailchimpsettings($params) {
   
   return civicrm_api3_create_success($groups);
 }
+
+/**
+ * CiviCRM to Mailchimp Sync
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */ 
+function civicrm_api3_mailchimp_sync($params) {
+  $result = array();
+  $runner = CRM_Mailchimp_Form_Sync::getRunner($params);
+  if ($runner) {
+    $result = $runner->runAll();
+  }
+
+  if ($result['is_error'] == 0) {
+    return civicrm_api3_create_success();
+  }
+  else {
+    return civicrm_api3_create_error();
+  }
+}
