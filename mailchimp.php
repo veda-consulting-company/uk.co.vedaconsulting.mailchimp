49d771bc6d1f883c9cca0cad38f9766f245886ef
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2025-02-11 21:47:35 +0100

    Runned civix upgrade to prevent smarty5 error.

--- mailchimp.php
+++ mailchimp.php
@@ -18,17 +18,6 @@ function mailchimp_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mailchimp_civicrm_xmlMenu(&$files) {
-  _mailchimp_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -64,86 +53,12 @@ function mailchimp_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- */
-function mailchimp_civicrm_postInstall() {
-  _mailchimp_civix_civicrm_postInstall();
-}
-
-/**
- * Implementation of hook_civicrm_uninstall
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function mailchimp_civicrm_uninstall() {
-  _mailchimp_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function mailchimp_civicrm_enable() {
   _mailchimp_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function mailchimp_civicrm_disable() {
-  return _mailchimp_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function mailchimp_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mailchimp_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mailchimp_civicrm_managed(&$entities) {
-  _mailchimp_civix_civicrm_managed($entities);
-}
-
-/**
- * Implementation of hook_civicrm_caseTypes
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailchimp_civicrm_caseTypes(&$caseTypes) {
-  _mailchimp_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implementation of hook_civicrm_alterSettingsFolders
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mailchimp_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mailchimp_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
